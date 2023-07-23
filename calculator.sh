df162b64d4f4132d52e5c3771320894dc20303cf
ckharia <chanchal.kharia@gmail.com>
2023-07-23 23:14:27 +0530

    some mchanges

--- calculator.sh
+++ calculator.sh
@@ -4,12 +4,19 @@ no1=$1
 no2=$2
 operation=$3
 
+function mul(){
+#sum=$no1*$no2
+sum=`expr $no1 * $no2`
+echo $mul
+}
+
 case $3 in
    1)
 	echo "I'll add $no1 & $no2"
+	mul
    ;;
    2)
-        echo "I'll substract $no1 & $no2"
+    #    echo "I'll substract $no1 & $no2"
    ;;
    3)
         echo "I'll multiply $no1 & $no2"
