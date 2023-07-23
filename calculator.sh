6deb3ce3900c242bc534bfd9343818b0b75621e8
ckharia <chanchal.kharia@gmail.com>
2023-07-23 23:33:44 +0530

    multi from chanchal

--- calculator.sh
+++ calculator.sh
@@ -5,22 +5,23 @@ no2=$2
 operation=$3
 
 function mul(){
-#sum=$no1*$no2
-sum=`expr $no1 * $no2`
+#mul=$no1*$no2
+mul=`expr $no1 * $no2`
 echo $mul
 }
 
 case $3 in
    1)
 	echo "I'll add $no1 & $no2"
-	mul
+	
    ;;
    2)
     #    echo "I'll substract $no1 & $no2"
    ;;
    3)
         echo "I'll multiply $no1 & $no2"
-   ;;   
+	mul   
+;;   
    4)
         echo "I'll divide $no1 & $no2"
    ;;
