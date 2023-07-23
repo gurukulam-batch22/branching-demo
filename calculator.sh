9053dc1bc3b05c1172517d7a35ea8e46dea6de3c
Sandeep Rawat <sandeep@opstree.com>
2023-07-23 13:20:40 +0530

    Create calculator.sh

---
+++ calculator.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+no1=$1
+no2=$2
+operation=$3
+
+case $3 in
+   1)
+	echo "I'll add $no1 & $no2"
+   ;;
+   2)
+        echo "I'll substract $no1 & $no2"
+   ;;
+   3)
+        echo "I'll multiply $no1 & $no2"
+   ;;   
+   4)
+        echo "I'll divide $no1 & $no2"
+   ;;
+esac
